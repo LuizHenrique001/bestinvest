e2c23726a335c86cb060fa192ff0909e0177747a
LuizHenrique <luizhenrique9x08@gmail.com>
2022-08-28 14:52:18 -0300

    Atualizado

--- js/javascript.js
+++ js/javascript.js
@@ -2,17 +2,17 @@ let dinheiro_investido_usuario = document.querySelector('p.dinheiro_investido_us
 let div_sair_erros = document.querySelector('div.div_sair_erros')
 
 let saldo_atual = document.querySelector('div.saldo_atual')
-
-let dinheiro_total = 0 // Valor total na conta não investido
 let valor_adicionar = 10000
+let dinheiro_total = 0 // Valor total na conta não investido
+dinheiro_total = valor_adicionar
 
-dinheiro_total = valor_adicionar//Já começa com 10k na conta
+//Já começa com 10k na conta se o valor for 0
 saldo_atual.innerHTML = 'R$'+`${dinheiro_total}`+',00'
+
 
 let div_adicionar_investimento = document.querySelector('div.div_adicionar_investimento')
 let investido_total = 0 // Investimento do usuário
-dinheiro_investido_usuario.innerHTML = 'R$'+`${JSON.
-    parse(localStorage.getItem('investido'))}`+',00'
+
 
 let valor_input_adicionar = document.querySelector('input.valor_adicionar')
 
@@ -40,11 +40,11 @@ function adicionarNovoInvestimento() {
     }else {
         div_sair_erros.innerHTML = ''
         dinheiro_total = parseFloat(dinheiro_total) - parseFloat(valor_input_formatado)
+
         investido_total += parseFloat(valor_input_formatado)
-        JSON.stringify(localStorage.setItem('investido', investido_total))
         saldo_atual.innerHTML = 'R$'+`${dinheiro_total}`+',00'
-        dinheiro_investido_usuario.innerHTML = 'R$'+`${JSON.
-            parse(localStorage.getItem('investido'))}`+',00'
+
+        dinheiro_investido_usuario.innerHTML = 'R$'+`${investido_total}`+',00'
 
         alert('Transação concluida!')
         div_adicionar_investimento.style.display = 'none'
@@ -72,7 +72,7 @@ function retirarInvestimento() {
             Campo vazio
         </p>`
     }
-    else if(valor_retirar_formatado > investido_total) {
+    else if(valor_retirar_formatado > parseFloat(investido_total)) {
         div_sair_erros_retirar.innerHTML = `
         <p class="saida_erro_adicionar">
             Saldo insuficiente
@@ -80,11 +80,44 @@ function retirarInvestimento() {
     }else {
         div_sair_erros_retirar = ''
         dinheiro_total = parseFloat(dinheiro_total) + parseFloat(valor_retirar_formatado)
-        investido_total = parseFloat(JSON.
-            parse(localStorage.getItem('investido'))) - parseFloat(valor_retirar_formatado)
+        investido_total -= parseFloat(valor_retirar_formatado)
+
         saldo_atual.innerHTML = 'R$'+`${dinheiro_total}`+',00'
-        dinheiro_investido_usuario.innerHTML = 'R$'+`${JSON.parse(localStorage.getItem('investido'))}`+',00'
+        dinheiro_investido_usuario.innerHTML = 'R$'+`${investido_total}`+',00'
         alert('Transação concluida!')
         div_retirar_investimento.style.display = 'none'
     }
+}
+
+// Opcoes de invetimento
+
+let btn_direto = document.querySelector('div#tesouro_direto')
+let btn_selic = document.querySelector('div#tesouro_selic')
+
+function aparecerOpcao_1() {
+    if(btn_direto.style.display == 'flex') {
+        btn_direto.style.display = 'none'
+    }else {
+        btn_direto.style.display = 'flex'
+    }
+}
+
+function aparecerOpcao_2() {
+    if(btn_selic.style.display == 'flex') {
+        btn_selic.style.display = 'none'
+    }else {
+        btn_selic.style.display = 'flex'
+    }
+}
+
+function opcaoRentabilidade(valor) {
+
+    tempo_rentabilidade = setInterval(function(){
+    investido_total = parseInt(investido_total) + (parseInt(investido_total) * valor / 100)
+    dinheiro_investido_usuario.innerHTML = 'R$'+`${investido_total}`
+    }, 2000)
+}
+
+function pararRentabilidade() {
+    clearInterval(tempo_rentabilidade)
 }
